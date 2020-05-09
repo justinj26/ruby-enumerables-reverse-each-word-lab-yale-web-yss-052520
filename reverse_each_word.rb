225067abca2de68bf470af81c715cede3ec97d27
Justin James <justin.james@yale.edu>
2020-05-09 15:58:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,11 +7,11 @@ def reverse_each_word(string)
  new_string_array.join(' ')
 end
 
-def reverse_each_word(string)
- string_array = string.split(" ")
-  string_array.collect do |element|
-    element.reverse 
-  end
-  string_array.join(' ')
-end
+#def reverse_each_word(string)
+# string_array = string.split(" ")
+ # string_array.collect do |element|
+  #  element.reverse 
+  #end
+  #string_array.join(' ')
+#end
 
